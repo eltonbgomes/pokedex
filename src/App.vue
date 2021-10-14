9b0a92cdd046d57ee8c9df91eb869b885b76d287
casa <eltonbarbosagomes@gmail.com>
2021-10-14 17:40:38 -0300

    primeira requisicao

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=1200&offset=0").then(res => {
+      console.log(res.data.results);
+    })
   }
 }
 </script>
